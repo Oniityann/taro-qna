6b4d71e89fca06b36b88b65596960d7a801b9829
Oniityann <zynfab26@hotmail.com>
2019-07-26 17:35:26 +0800

    less layout
    less layout

--- qna/src/pages/index/index.js
+++ qna/src/pages/index/index.js
@@ -1,28 +1,28 @@
-import Taro, { Component } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
-import './index.less'
+import Taro, { Component } from '@tarojs/taro';
+import { View, Text, Button } from '@tarojs/components';
+import './index.less';
 
 export default class Index extends Component {
-
   config = {
     navigationBarTitleText: '首页'
-  }
+  };
 
-  componentWillMount () { }
+  componentWillMount() {}
 
-  componentDidMount () { }
+  componentDidMount() {}
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+        <View className='title'>Taro Question & Answers</View>
+        <Button className='question-button'>Ask</Button>
       </View>
-    )
+    );
   }
 }
